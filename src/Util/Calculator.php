5e74aa88d4cd924aaabab5f14c4d6c1acf9adfa7
erkkki <erkheikki@hotmail.com>
2021-04-09 23:42:38 +0300

    actions codesniffer psr12

--- src/Util/Calculator.php
+++ src/Util/Calculator.php
@@ -1,11 +1,12 @@
 <?php
+
 // src/Util/Calculator.php
 namespace App\Util;
 
 class Calculator
 {
-  public function add(int $a,int $b): int
-  {
-    return $a + $b;
-  }
+    public function add(int $a, int $b): int
+    {
+        return $a + $b;
+    }
 }
